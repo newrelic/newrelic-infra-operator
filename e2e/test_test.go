21dab7aefe25df34adf5acefafa21151271ee91b
paologallinaharbur <paologallina1992@gmail.com>
2021-04-30 16:57:04 +0200

    feat(e2e): adding initial e2e tests

--- e2e/test_test.go
+++ e2e/test_test.go
@@ -7,12 +7,17 @@ package e2e_test
 
 import (
 	"context"
+	"math/rand"
 	"os"
 	"testing"
+	"time"
 
+	appv1 "k8s.io/api/apps/v1"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/utils/pointer"
 )
 
 // This test should cover the following functionality:
@@ -43,4 +48,168 @@ func Test_Example_test(t *testing.T) {
 	for _, pod := range pods.Items {
 		t.Logf("pod name: %q", pod.Name)
 	}
+}
+
+//nolint:funlen
+func Test_injection_pod(t *testing.T) {
+	t.Parallel()
+
+	t.Run("succeed", func(t *testing.T) {
+		t.Parallel()
+		// use the current context in kubeconfig
+
+		config, err := clientcmd.BuildConfigFromFlags("", os.Getenv("KUBECONFIG"))
+		if err != nil {
+			t.Fatalf("building config from kubeconfig: %v", err)
+		}
+
+		clientset, err := kubernetes.NewForConfig(config)
+		if err != nil {
+			t.Fatalf("preparing clientset: %v", err)
+		}
+
+		// Crating disposable namespace
+		namespaceName := RandStringRunes(8)
+		namespace := v1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: namespaceName}}
+		_, err = clientset.CoreV1().Namespaces().Create(context.Background(), &namespace, metav1.CreateOptions{})
+		if err != nil {
+			t.Fatalf("creating namespace: %v", err)
+		}
+		//nolint:errcheck
+		defer clientset.CoreV1().Namespaces().Delete(context.Background(), namespaceName, metav1.DeleteOptions{})
+
+		// Pod to be created
+		pod := v1.Pod{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "test-pod",
+				Namespace: namespaceName,
+				Labels: map[string]string{
+					"eks.amazonaws.com/fargate-profile": "testprofile",
+				},
+			},
+			Spec: v1.PodSpec{
+				Containers: []v1.Container{
+					{
+						Name:  "test-nginx",
+						Image: "nginx",
+					},
+				},
+			},
+		}
+		_, err = clientset.CoreV1().Pods(namespaceName).Create(context.Background(), &pod, metav1.CreateOptions{})
+		if err != nil {
+			t.Fatalf("creating pod: %v", err)
+		}
+
+		pCreated, err := clientset.CoreV1().Pods(namespaceName).Get(context.TODO(), "test-pod", metav1.GetOptions{})
+		if err != nil {
+			t.Fatalf("getting pod: %v", err)
+		}
+		if len(pCreated.Spec.Containers) != 2 {
+			t.Fatalf("expecting 2 containers: %v", err)
+		}
+		if len(pCreated.Spec.Volumes) != 5 {
+			t.Fatalf("expecting 4 volumes: %v", err)
+		}
+		if pCreated.ObjectMeta.Labels["newrelic/agent-injected"] != "true" {
+			t.Fatalf("expecting newrelic/agent-injected label to be injected: %v", err)
+		}
+	})
+}
+
+//nolint:funlen
+func Test_injection_Deployment(t *testing.T) {
+	t.Parallel()
+
+	t.Run("succeed", func(t *testing.T) {
+		t.Parallel()
+		// use the current context in kubeconfig
+
+		config, err := clientcmd.BuildConfigFromFlags("", os.Getenv("KUBECONFIG"))
+		if err != nil {
+			t.Fatalf("building config from kubeconfig: %v", err)
+		}
+
+		clientset, err := kubernetes.NewForConfig(config)
+		if err != nil {
+			t.Fatalf("preparing clientset: %v", err)
+		}
+
+		// Crating disposable namespace
+		namespaceName := RandStringRunes(8)
+		namespace := v1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: namespaceName}}
+		_, err = clientset.CoreV1().Namespaces().Create(context.Background(), &namespace, metav1.CreateOptions{})
+		if err != nil {
+			t.Fatalf("creating namespace: %v", err)
+		}
+		//nolint:errcheck
+		defer clientset.CoreV1().Namespaces().Delete(context.Background(), namespaceName, metav1.DeleteOptions{})
+
+		// Deploy to be created
+		deploy := appv1.Deployment{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "test-deploy",
+				Namespace: namespaceName,
+				Labels: map[string]string{
+					"app": "nginx",
+				},
+			},
+			Spec: appv1.DeploymentSpec{
+				Replicas: pointer.Int32Ptr(2),
+				Selector: &metav1.LabelSelector{
+					MatchLabels: map[string]string{
+						"app": "nginx",
+					},
+				},
+				Template: v1.PodTemplateSpec{
+					ObjectMeta: metav1.ObjectMeta{
+						Labels: map[string]string{
+							"app": "nginx",
+						},
+					},
+					Spec: v1.PodSpec{
+						Containers: []v1.Container{
+							{
+								Name:  "test-nginx",
+								Image: "nginx",
+							},
+						},
+					},
+				},
+			},
+		}
+		_, err = clientset.AppsV1().Deployments(namespaceName).Create(context.Background(), &deploy, metav1.CreateOptions{})
+		if err != nil {
+			t.Fatalf("creating deployment: %v", err)
+		}
+
+		pList, err := clientset.CoreV1().Pods(namespaceName).List(context.Background(), metav1.ListOptions{})
+		if err != nil {
+			t.Fatalf("getting list of pods: %v", err)
+		}
+		for _, p := range pList.Items {
+			if len(p.Spec.Containers) != 2 {
+				t.Fatalf("expecting 2 containers: %v", err)
+			}
+			if len(p.Spec.Volumes) != 5 {
+				t.Fatalf("expecting 4 volumes: %v", err)
+			}
+			if p.ObjectMeta.Labels["newrelic/agent-injected"] != "true" {
+				t.Fatalf("expecting newrelic/agent-injected label to be injected: %v", err)
+			}
+		}
+	})
+}
+
+func RandStringRunes(n int) string {
+	letterRunes := []rune("abcdefghijklmnopqrstuvwxyz")
+
+	rand.Seed(time.Now().UnixNano())
+
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+
+	return string(b)
 }
