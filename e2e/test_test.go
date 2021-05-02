55dfb010d96b901b939a4da3d94c9b21c34ed060
Mateusz Gozdek <mateusz@kinvolk.io>
2021-05-02 20:51:55 +0200

    e2e: remove example test
    It was only added as a placeholder, so now that we have other tests, it
    can be removed.
    
    Signed-off-by: Mateusz Gozdek <mateusz@kinvolk.io>

--- e2e/test_test.go
+++ e2e/test_test.go
@@ -26,30 +26,7 @@ import (
 // - Pod with exclude mark (label/annotation?) do not get agent injected.
 // - Pod without exclude mark get agent sidecar container injected. This verifies:
 //   - Operator has ability to grant required permission to pods?
-func Test_Example_test(t *testing.T) {
-	t.Parallel()
-
-	// use the current context in kubeconfig
-	config, err := clientcmd.BuildConfigFromFlags("", os.Getenv("KUBECONFIG"))
-	if err != nil {
-		t.Fatalf("building config from kubeconfig: %v", err)
-	}
-
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		t.Fatalf("preparing clientset: %v", err)
-	}
-
-	pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
-	if err != nil {
-		t.Fatalf("setting list of pods: %v", err)
-	}
-
-	for _, pod := range pods.Items {
-		t.Logf("pod name: %q", pod.Name)
-	}
-}
-
+//
 //nolint:funlen,cyclop
 func Test_clusterRoleBinding_Update(t *testing.T) {
 	t.Parallel()
