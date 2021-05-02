10f0e38b54d515e4f84dd47d6210afdafe062f20
Mateusz Gozdek <mateusz@kinvolk.io>
2021-05-02 20:53:56 +0200

    e2e: re-implement tests to make them functionality-focused
    Rather than testing the specific pieces of functionality, perform
    general tests focused on core features of infrastructure agent injector,
    like granting privileges, ensuring all Pod dependencies are satisfied
    (e.g. Secrets) etc, which should provide very broad coverage of
    functionality (e.g. generation of webhook certificates) while not making
    the tests fragile (they are less likely to break if changes to the
    implementation are introduced, e.g. the way the license is fetched for
    pod).
    
    This commit also changes the implementation to use envtest, similarly to
    integration tests, so we don't need to read KUBECONFIG environment
    variable directly, but if needed, envtest can spawn it's own test
    environment (though this won't fully work, as we do not deploy the
    resources there).
    
    Signed-off-by: Mateusz Gozdek <mateusz@kinvolk.io>

--- e2e/test_test.go
+++ e2e/test_test.go
@@ -3,272 +3,210 @@
 
 // +build e2e
 
+// Package e2e_test implements e2e tests for operator, which are not related to any specific package.
 package e2e_test
 
 import (
 	"context"
+	"fmt"
 	"math/rand"
-	"os"
+	"strings"
 	"testing"
 	"time"
 
-	appv1 "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/utils/pointer"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+
+	"github.com/newrelic/newrelic-infra-operator/internal/testutil"
 )
 
-// This test should cover the following functionality:
+const (
+	testPrefix = "newrelic-infra-operator-tests"
+)
+
+// Test_Infra_agent_injection_webhook test covers the following functionality:
 // - Pod gets created after mutating webhook is installed. This verifies:
 //   - TLS certificate generation.
 // - Pod with exclude mark (label/annotation?) do not get agent injected.
 // - Pod without exclude mark get agent sidecar container injected. This verifies:
-//   - Operator has ability to grant required permission to pods?
+//   - Operator has ability to grant required permission to pods.
 //
 //nolint:funlen,cyclop
-func Test_clusterRoleBinding_Update(t *testing.T) {
+func Test_Infra_agent_injection_webhook(t *testing.T) {
 	t.Parallel()
 
-	t.Run("succeed", func(t *testing.T) {
+	clientset := testClientset(t)
+
+	ctx := testutil.ContextWithDeadline(t)
+
+	testEndpoints := []string{
+		"metrics",
+		"metrics/cadvisor",
+		"stats/summary",
+		"pods",
+	}
+
+	testCommands := []string{"set -xe"}
+
+	for _, endpoint := range testEndpoints {
+		uri := fmt.Sprintf("/api/v1/nodes/%s/proxy/%s", randomNodeName(ctx, t, clientset), endpoint)
+		testCommands = append(testCommands, fmt.Sprintf("kubectl get --raw %s", uri))
+	}
+
+	podTemplate := v1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			GenerateName: testPrefix,
+		},
+		Spec: v1.PodSpec{
+			InitContainers: []v1.Container{
+				{
+					Name:    "kubectl",
+					Image:   "bitnami/kubectl",
+					Command: []string{"sh", "-c", strings.Join(testCommands, "\n")},
+				},
+			},
+			Containers: []v1.Container{
+				{
+					Name:  "nginx",
+					Image: "nginx",
+				},
+			},
+		},
+	}
+
+	podClient := clientset.CoreV1().Pods(testNamespace(ctx, t, clientset))
+
+	pod, err := podClient.Create(ctx, &podTemplate, metav1.CreateOptions{})
+	if err != nil {
+		t.Fatalf("creating pod: %v", err)
+	}
+
+	podName := pod.Name
+
+	// To simulate what infrastructure agent will be doing.
+	t.Run("allows_mutated_pod_to_fetch_node_metrics_via_kubernetes_API_proxy", func(t *testing.T) {
 		t.Parallel()
-		// use the current context in kubeconfig
 
-		config, err := clientcmd.BuildConfigFromFlags("", os.Getenv("KUBECONFIG"))
-		if err != nil {
-			t.Fatalf("building config from kubeconfig: %v", err)
-		}
+		for {
+			pod, err := podClient.Get(ctx, podName, metav1.GetOptions{})
+			if err != nil {
+				t.Fatalf("getting pod %q: %v", podName, err)
+			}
 
-		clientset, err := kubernetes.NewForConfig(config)
-		if err != nil {
-			t.Fatalf("preparing clientset: %v", err)
-		}
+			if len(pod.Status.InitContainerStatuses) == 0 {
+				t.Logf("pod has no init container status yet")
 
-		// Crating disposable namespace
-		namespaceName := RandStringRunes(8)
-		namespace := v1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: namespaceName}}
-		_, err = clientset.CoreV1().Namespaces().Create(context.Background(), &namespace, metav1.CreateOptions{})
-		if err != nil {
-			t.Fatalf("creating namespace: %v", err)
-		}
-		//nolint:errcheck
-		defer clientset.CoreV1().Namespaces().Delete(context.Background(), namespaceName, metav1.DeleteOptions{})
+				time.Sleep(1 * time.Second)
 
-		serviceAccountName := RandStringRunes(8)
+				continue
+			}
 
-		_, err = clientset.CoreV1().ServiceAccounts(namespaceName).Create(context.Background(),
-			&v1.ServiceAccount{ObjectMeta: metav1.ObjectMeta{Name: serviceAccountName}}, metav1.CreateOptions{})
-		if err != nil {
-			t.Fatalf("creating service account: %v", err)
-		}
+			state := pod.Status.InitContainerStatuses[0].State.Terminated
+			if state == nil {
+				t.Logf("container has not terminated yet %v", pod.Status.InitContainerStatuses[0].State)
 
-		// Pod to be created
-		pod := v1.Pod{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "testpod",
-				Namespace: namespaceName,
-				Labels: map[string]string{
-					"eks.amazonaws.com/fargate-profile": "testprofile",
-				},
-			},
-			Spec: v1.PodSpec{
-				ServiceAccountName: serviceAccountName,
-				Containers: []v1.Container{
-					{
-						Name:  "test-nginx",
-						Image: "nginx",
-					},
-				},
-			},
-		}
-		_, err = clientset.CoreV1().Pods(namespaceName).Create(context.Background(), &pod, metav1.CreateOptions{})
-		if err != nil {
-			t.Fatalf("creating pod: %v", err)
-		}
+				time.Sleep(1 * time.Second)
 
-		crb, err := clientset.RbacV1().ClusterRoleBindings().
-			Get(context.Background(), "newrelic-infra-operator-infra-agent", metav1.GetOptions{})
-		if err != nil {
-			t.Fatalf("getting crb: %v", err)
+				continue
+			}
+
+			if state.ExitCode != 0 {
+				t.Fatalf("test container exited with code %d", state.ExitCode)
+			}
+
+			break
 		}
+	})
+
+	t.Run("adds_infrastructure_agent_sidecar_container_to_pod", func(t *testing.T) {
+		t.Parallel()
+
 		found := false
-		for _, s := range crb.Subjects {
-			if s.Name == serviceAccountName && s.Namespace == namespaceName {
+
+		if len(pod.Spec.Containers) == 1 {
+			t.Fatalf("no sidecar container injected to pod")
+		}
+
+		expectedName := "newrelic-infrastructure"
+
+		for _, c := range pod.Spec.Containers {
+			if strings.Contains(c.Name, expectedName) {
 				found = true
 			}
 		}
+
 		if !found {
-			t.Fatalf("crb does not contain the pod service account")
+			t.Fatalf("no container with name including %q found in injected pod", expectedName)
 		}
 	})
 }
 
-//nolint:funlen
-func Test_injection_pod(t *testing.T) {
-	t.Parallel()
+func testClientset(t *testing.T) *kubernetes.Clientset {
+	t.Helper()
 
-	t.Run("succeed", func(t *testing.T) {
-		t.Parallel()
-		// use the current context in kubeconfig
+	testEnv := &envtest.Environment{}
 
-		config, err := clientcmd.BuildConfigFromFlags("", os.Getenv("KUBECONFIG"))
-		if err != nil {
-			t.Fatalf("building config from kubeconfig: %v", err)
-		}
-
-		clientset, err := kubernetes.NewForConfig(config)
-		if err != nil {
-			t.Fatalf("preparing clientset: %v", err)
-		}
-
-		// Crating disposable namespace
-		namespaceName := RandStringRunes(8)
-		namespace := v1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: namespaceName}}
-		_, err = clientset.CoreV1().Namespaces().Create(context.Background(), &namespace, metav1.CreateOptions{})
-		if err != nil {
-			t.Fatalf("creating namespace: %v", err)
-		}
-		//nolint:errcheck
-		defer clientset.CoreV1().Namespaces().Delete(context.Background(), namespaceName, metav1.DeleteOptions{})
-
-		// Pod to be created
-		pod := v1.Pod{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "testpod",
-				Namespace: namespaceName,
-				Labels: map[string]string{
-					"eks.amazonaws.com/fargate-profile": "testprofile",
-				},
-			},
-			Spec: v1.PodSpec{
-				Containers: []v1.Container{
-					{
-						Name:  "test-nginx",
-						Image: "nginx",
-					},
-				},
-			},
-		}
-		_, err = clientset.CoreV1().Pods(namespaceName).Create(context.Background(), &pod, metav1.CreateOptions{})
-		if err != nil {
-			t.Fatalf("creating pod: %v", err)
-		}
-
-		pCreated, err := clientset.CoreV1().Pods(namespaceName).Get(context.TODO(), "testpod", metav1.GetOptions{})
-		if err != nil {
-			t.Fatalf("getting pod: %v", err)
-		}
-		if len(pCreated.Spec.Containers) != 2 {
-			t.Fatalf("expecting 2 containers: %v", err)
-		}
-		if len(pCreated.Spec.Volumes) != 5 {
-			t.Fatalf("expecting 4 volumes: %v", err)
-		}
-		if _, ok := pCreated.ObjectMeta.Labels["newrelic/agent-injected"]; !ok {
-			t.Fatalf("expecting newrelic/agent-injected label to be injected")
-		}
-	})
-}
-
-//nolint:funlen
-func Test_injection_Deployment(t *testing.T) {
-	t.Parallel()
-
-	t.Run("succeed", func(t *testing.T) {
-		t.Parallel()
-		// use the current context in kubeconfig
-
-		config, err := clientcmd.BuildConfigFromFlags("", os.Getenv("KUBECONFIG"))
-		if err != nil {
-			t.Fatalf("building config from kubeconfig: %v", err)
-		}
-
-		clientset, err := kubernetes.NewForConfig(config)
-		if err != nil {
-			t.Fatalf("preparing clientset: %v", err)
-		}
-
-		// Crating disposable namespace
-		namespaceName := RandStringRunes(8)
-		namespace := v1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: namespaceName}}
-		_, err = clientset.CoreV1().Namespaces().Create(context.Background(), &namespace, metav1.CreateOptions{})
-		if err != nil {
-			t.Fatalf("creating namespace: %v", err)
-		}
-		//nolint:errcheck
-		defer clientset.CoreV1().Namespaces().Delete(context.Background(), namespaceName, metav1.DeleteOptions{})
-
-		// Deploy to be created
-		deploy := appv1.Deployment{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "test-deploy",
-				Namespace: namespaceName,
-				Labels: map[string]string{
-					"app": "nginx",
-				},
-			},
-			Spec: appv1.DeploymentSpec{
-				Replicas: pointer.Int32Ptr(2),
-				Selector: &metav1.LabelSelector{
-					MatchLabels: map[string]string{
-						"app": "nginx",
-					},
-				},
-				Template: v1.PodTemplateSpec{
-					ObjectMeta: metav1.ObjectMeta{
-						Labels: map[string]string{
-							"app": "nginx",
-						},
-					},
-					Spec: v1.PodSpec{
-						Containers: []v1.Container{
-							{
-								Name:  "test-nginx",
-								Image: "nginx",
-							},
-						},
-					},
-				},
-			},
-		}
-		_, err = clientset.AppsV1().Deployments(namespaceName).Create(context.Background(), &deploy, metav1.CreateOptions{})
-		if err != nil {
-			t.Fatalf("creating deployment: %v", err)
-		}
-
-		pList, err := clientset.CoreV1().Pods(namespaceName).List(context.Background(), metav1.ListOptions{})
-		if err != nil {
-			t.Fatalf("getting list of pods: %v", err)
-		}
-		for _, p := range pList.Items {
-			if len(p.Spec.Containers) != 2 {
-				t.Fatalf("expecting 2 containers: %v", err)
-			}
-			if len(p.Spec.Volumes) != 5 {
-				t.Fatalf("expecting 4 volumes: %v", err)
-			}
-			if _, ok := p.ObjectMeta.Labels["newrelic/agent-injected"]; !ok {
-				t.Fatalf("expecting newrelic/agent-injected label to be injected")
-			}
-		}
-	})
-}
-
-func RandStringRunes(n int) string {
-	letterRunes := []rune("abcdefghijklmnopqrstuvwxyz")
-
-	rand.Seed(time.Now().UnixNano())
-
-	b := make([]rune, n)
-	for i := range b {
-		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	cfg, err := testEnv.Start()
+	if err != nil {
+		t.Fatalf("starting test environment: %v", err)
 	}
 
-	return string(b)
+	t.Cleanup(func() {
+		if err := testEnv.Stop(); err != nil {
+			t.Logf("stopping test environment: %v", err)
+		}
+	})
+
+	clientSet, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		t.Fatalf("creating clientset: %v", err)
+	}
+
+	return clientSet
 }
 
-//nolint:gochecknoinits
-func init() {
-	rand.Seed(time.Now().UnixNano())
+func testNamespace(ctx context.Context, t *testing.T, clientset *kubernetes.Clientset) string {
+	t.Helper()
+
+	namespaceTemplate := v1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			GenerateName: testPrefix,
+		},
+	}
+
+	namespaceClient := clientset.CoreV1().Namespaces()
+
+	ns, err := namespaceClient.Create(ctx, &namespaceTemplate, metav1.CreateOptions{})
+	if err != nil {
+		t.Fatalf("creating namespace: %v", err)
+	}
+
+	namespaceName := ns.Name
+
+	t.Cleanup(func() {
+		if err := namespaceClient.Delete(ctx, namespaceName, metav1.DeleteOptions{}); err != nil {
+			t.Logf("deleting test namespace %q: %v", ns.Name, err)
+		}
+	})
+
+	return namespaceName
+}
+
+func randomNodeName(ctx context.Context, t *testing.T, clientset *kubernetes.Clientset) string {
+	t.Helper()
+
+	rand.Seed(time.Now().Unix())
+
+	nodes, err := clientset.CoreV1().Nodes().List(ctx, metav1.ListOptions{})
+	if err != nil {
+		t.Fatalf("listing nodes: %v", err)
+	}
+
+	if len(nodes.Items) == 0 {
+		t.Fatalf("no Node objects found")
+	}
+
+	return nodes.Items[rand.Intn(len(nodes.Items))].Name
 }
