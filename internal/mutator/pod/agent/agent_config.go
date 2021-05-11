d128f3200aa6323bd2e5e0571bbc3df0a0ca046c
paologallinaharbur <paologallina1992@gmail.com>
2021-05-11 15:04:06 +0200

    fix(tags): json tags should be used in place of yaml ones

--- internal/mutator/pod/agent/agent_config.go
+++ internal/mutator/pod/agent/agent_config.go
@@ -12,29 +12,29 @@ import (
 // InfraAgentConfig holds the user's configuration for the sidecar to be injected.
 type InfraAgentConfig struct {
 	// Here we can map the whole user configuration from helm chart.
-	ExtraEnvVars           map[string]string  `yaml:"extraEnvVars"`
-	ResourcesWithSelectors []Resource         `yaml:"resourcesWithSelectors"`
-	Image                  Image              `yaml:"image"`
-	PodSecurityContext     PodSecurityContext `yaml:"podSecurityContext"`
+	ExtraEnvVars           map[string]string  `json:"extraEnvVars"`
+	ResourcesWithSelectors []Resource         `json:"resourcesWithSelectors"`
+	Image                  Image              `json:"image"`
+	PodSecurityContext     PodSecurityContext `json:"podSecurityContext"`
 }
 
 // Image config.
 type Image struct {
-	Repository string            `yaml:"repository"`
-	Tag        string            `yaml:"tag"`
-	PullPolicy corev1.PullPolicy `yaml:"pullPolicy"`
+	Repository string            `json:"repository"`
+	Tag        string            `json:"tag"`
+	PullPolicy corev1.PullPolicy `json:"pullPolicy"`
 }
 
 // PodSecurityContext config.
 type PodSecurityContext struct {
-	RunAsUser  int64 `yaml:"runAsUser"`
-	RunAsGroup int64 `yaml:"runAsGroup"`
+	RunAsUser  int64 `json:"runAsUser"`
+	RunAsGroup int64 `json:"runAsGroup"`
 }
 
 // Resource config.
 type Resource struct {
-	ResourceRequirements corev1.ResourceRequirements
-	LabelSelector        metav1.LabelSelector
+	ResourceRequirements corev1.ResourceRequirements `json:"resourceRequirements"`
+	LabelSelector        metav1.LabelSelector        `json:"labelSelector"`
 
-	selector labels.Selector
+	selector labels.Selector `json:"-"`
 }
